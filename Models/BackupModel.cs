78ead0d1cf09ae0978b44d1c221f47077b912c4d
Yanis ♨️ <azellaxmc@gmail.com>
2024-02-05 15:15:14 +0100

    Add a body for the code

--- Models/BackupModel.cs
+++ Models/BackupModel.cs
@@ -4,18 +4,84 @@ using System.IO;
 
 namespace EasySaveConsole
 {
+        
+    // BackupJob main class
     public class BackupJob
     {
-        public string name;
-        public string sourceDir;
-        public string destinationDir;
-        public string type;
+        public string Name { get; set; }
+        public string SourceDir { get; set; }
+        public string DestinationDir { get; set; }
+        public string Type { get; set; } // Complet ou Différentiel
 
-        public void Save()
+        public BackupJob(string name, string sourceDir, string destinationDir, string type)
         {
-            // Logique pour exécuter la sauvegarde
-            // TODO
+            Name = name;
+            SourceDir = sourceDir;
+            DestinationDir = destinationDir;
+            Type = type;
         }
     }
 
+    // BackupExec class (inherits from BackupJob)
+    public class BackupExec : BackupJob
+    {
+        public BackupExec(string name, string sourceDir, string destinationDir, string type)
+            : base(name, sourceDir, destinationDir, type) { }
+
+        public void Save()
+        {
+            // TODO -> implement save logic here
+        }
+    }
+
+    // BackupManager class (inherits from BackupJob)
+    public class BackupManager
+    {
+        private List<BackupJob> backupJobs;
+
+        public BackupManager()
+        {
+            backupJobs = new List<BackupJob>();
+            LoadJobsFromJson(); // Load BackupJobs from JSON file
+        }
+
+        public void Create(BackupJob job)
+        {
+            backupJobs.Add(job);
+            SaveJobsToJson();
+        }
+
+        public void Edit(string jobName, BackupJob updatedJob)
+        {
+            var job = backupJobs.Find(j => j.Name == jobName);
+            if (job != null)
+            {
+                backupJobs.Remove(job);
+                backupJobs.Add(updatedJob);
+                SaveJobsToJson();
+            }
+            // TODO (handle error)
+        }
+
+        public void Delete(string jobName)
+        {
+            var job = backupJobs.Find(j => j.Name == jobName);
+            if (job != null)
+            {
+                backupJobs.Remove(job);
+                SaveJobsToJson();
+            }
+            // TODO (handle error)
+        }
+
+        private void SaveJobsToJson()
+        {
+            // TODO
+        }
+
+        private void LoadJobsFromJson()
+        {
+            // TODO
+        }
+    }
 }
