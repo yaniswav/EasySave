298df99408b0e9ab94aeec011551ab86cb88a6d2
noano <noan.ouedraogo@gmail.com>
2024-02-06 10:32:03 +0100

    Normalement c'est carré peut etre modif des noms jsp

--- Models/LoggingModel.cs
+++ Models/LoggingModel.cs
@@ -1,18 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
 
 namespace EasySaveConsole
 {
-    
-    public class LoggingModel
+    public class LogEntry
     {
+        public DateTime Timestamp { get; set; }
+        public string BackupName { get; set; }
+        public string Action { get; set; }
+        public string Details { get; set; }
 
-        private string logPath;
-
-        public void Write()
+        public LogEntry(string backupName, string action, string details)
         {
-            //TODO
+            Timestamp = DateTime.Now;
+            BackupName = backupName;
+            Action = action;
+            Details = details;
+        }
+    }
+
+    public class LogModel
+    {
+        private readonly string _logFilePath;
+        private readonly List<LogEntry> _logEntries;
+
+        public LogModel(string logFilePath)
+        {
+            _logFilePath = logFilePath;
+            _logEntries = new List<LogEntry>();
+        }
+
+        public void AddEntry(string backupName, string action, string details)
+        {
+            var logEntry = new LogEntry(backupName, action, details);
+            _logEntries.Add(logEntry);
+            WriteLogToFile(logEntry);
+        }
+
+        private void WriteLogToFile(LogEntry logEntry)
+        {
+            string json = JsonSerializer.Serialize(logEntry, new JsonSerializerOptions { WriteIndented = true });
+            File.AppendAllText(_logFilePath, json + Environment.NewLine);
         }
     }
 }
