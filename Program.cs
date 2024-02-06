85ee55b37262623caaf0bd136284de1770d9c419
noano <noan.ouedraogo@gmail.com>
2024-02-06 11:06:04 +0100

    Messages console ok, Program adapté à View

--- Program.cs
+++ Program.cs
@@ -6,10 +6,15 @@ namespace EasySaveConsole
     {
         public static void Main(string[] args)
         {
-            View view = new View();
+            // Supposons que BackupManager est une classe ou un composant dans ConfigModel qui gère les sauvegardes.
+            // Vous devez remplacer cette partie par la création réelle de votre gestionnaire de sauvegarde.
+            ConfigModel.BackupManager backupManager = new ConfigModel.BackupManager();
 
-            // starting our app
-            view.StartInterface();
+            // Création de la vue avec le gestionnaire de sauvegarde.
+            View view = new View(backupManager);
+
+            // Démarrage de l'interface utilisateur.
+            view.DisplayMenu();
         }
     }
 }
