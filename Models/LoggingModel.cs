642c16e0637bc814005e60bdfb216acba11d3948
SUBASINGHAM Pirinthika <ks.pirinthika@gmail.com>
2024-02-07 10:28:00 +0100

    Add comment LoggingModel.cs

--- Models/LoggingModel.cs
+++ Models/LoggingModel.cs
@@ -3,8 +3,12 @@ using System.IO;
 using System.Text.Json;
 using System.Collections.Generic;
 
+//Define namespace to organize classes related to logs functionality
+
 namespace EasySaveConsole
 {
+    //Class for LogEntry 
+    
     public class LogEntry
     {
         public DateTime Timestamp { get; set; }
@@ -12,6 +16,8 @@ namespace EasySaveConsole
         public string Action { get; set; }
         public string Details { get; set; }
 
+        //Constructor to initialize new entry in log with specified details 
+        
         public LogEntry(string backupName, string action, string details)
         {
             Timestamp = DateTime.Now;
@@ -21,17 +27,24 @@ namespace EasySaveConsole
         }
     }
 
+    //Class to manage log model and log writing entries
+    
     public class LogModel
     {
         private readonly string _logFilePath;
         private readonly List<LogEntry> _logEntries;
 
+        
+        //Constructor to initialize log model with specified path
+        
         public LogModel(string logFilePath)
         {
             _logFilePath = logFilePath;
             _logEntries = new List<LogEntry>();
         }
 
+        //Method to add a new entry in log and file entry
+        
         public void AddEntry(string backupName, string action, string details)
         {
             var logEntry = new LogEntry(backupName, action, details);
@@ -39,6 +52,8 @@ namespace EasySaveConsole
             WriteLogToFile(logEntry);
         }
 
+        //Private method to write an log entry in LogFile
+        
         private void WriteLogToFile(LogEntry logEntry)
         {
             string json = JsonSerializer.Serialize(logEntry, new JsonSerializerOptions { WriteIndented = true });
