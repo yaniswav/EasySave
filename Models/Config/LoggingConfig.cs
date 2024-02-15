370f3a556e9bfafd2a08df51a0f7244f0e6217fc
yaniswav <azellaxmc@gmail.com>
2024-02-15 21:11:06 +0100

    Updated Logging system

---
+++ Models/Config/LoggingConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+
+namespace EasySave;
+
+public static class ConfigurationHelper
+{
+    public static string GetOutputFormat()
+    {
+        return ConfigurationManager.AppSettings["OutputFormat"];
+    }
+
+    public static void SetOutputFormat(string format)
+    {
+        // Get the current configuration file
+        Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+        // Modify the OutputFormat setting
+        config.AppSettings.Settings["OutputFormat"].Value = format;
+
+        // Save the configuration file
+        config.Save(ConfigurationSaveMode.Modified);
+
+        // Refresh the appSettings section to reflect updated settings
+        ConfigurationManager.RefreshSection("appSettings");
+    }
+}
