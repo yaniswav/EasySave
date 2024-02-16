6b76526747c55349de0c1801b7edc31de88940a8
SUBASINGHAM Pirinthika <ks.pirinthika@gmail.com>
2024-02-16 11:26:26 +0100

    Rename CryptoSoft.cs as CryptoSoftChecker.cs

--- Resources/CryptoSoft/CryptoSoft.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EasySave;
-
-// Adding cryptosoft utilities to encrypt files
-public class CryptoSoft
-{
-    
-}
---
+++ Resources/CryptoSoft/CryptoSoftChecker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+//namespace EasySave;
+
+// Verify is cryptosoft is installed or not
+public class CryptosoftChecker
+{
+    public bool IsCryptosoftInstalled()
+    {
+        try
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "cryptosoft.exe", // Name of executable file
+            };
+
+            using (Process process = Process.Start(startInfo))
+            {
+                process.WaitForExit();
+                
+                if (process.ExitCode == 0)
+                {
+                    return true;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erreur lors de la vérification de cryptosoft: {ex.Message}");
+        }
+
+        return false; // Return false because cryptosoft wasn't find or not installed
+}
