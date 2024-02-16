d4f424ca6d23a455046d67215d5939c5ee9be9b1
Matisse <matisse.appere@viacesi.fr>
2024-02-16 10:12:32 +0100

    mise à jour de ViewModels

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,83 @@
-﻿namespace EasySave.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using EasySaveConsole;
 
-public class MainWindowViewModel : ViewModelBase
+namespace EasySave.ViewModels
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private readonly BackupManager _backupManager;
+
+        public ObservableCollection<BackupJob> BackupJobs { get; }
+
+        public ICommand StartBackupCommand { get; }
+        public ICommand LoadBackupJobsCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            BackupJobs = new ObservableCollection<BackupJob>();
+            _backupManager = new BackupManager();
+
+            // Utilisation de la méthode correcte pour charger les jobs de sauvegarde
+            LoadBackupJobs();
+
+            LoadBackupJobsCommand = new RelayCommand(_ => LoadBackupJobs());
+            StartBackupCommand = new RelayCommand(StartBackup, CanStartBackup);
+        }
+
+        // Méthode pour charger les jobs de sauvegarde à partir de BackupManager
+        private void LoadBackupJobs()
+        {
+            BackupJobs.Clear();
+            _backupManager.LoadBackupJobs(); // Charge les jobs à partir de la configuration
+            foreach (var job in _backupManager.Jobs) // Utilise la nouvelle propriété publique Jobs de BackupManager
+            {
+                BackupJobs.Add(job);
+            }
+        }
+
+        private bool CanStartBackup(object parameter)
+        {
+            // Logique pour déterminer si la commande StartBackup peut s'exécuter
+            return parameter is string jobName && !string.IsNullOrWhiteSpace(jobName);
+        }
+
+        private void StartBackup(object parameter)
+        {
+            if (parameter is string jobName)
+            {
+                _backupManager.ExecuteJobs(new string[] { jobName });
+            }
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute?.Invoke(parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
 }
--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -1,7 +1,17 @@
-﻿using ReactiveUI;
+﻿using System.ComponentModel;
+using ReactiveUI;
 
-namespace EasySave.ViewModels;
-
-public class ViewModelBase : ReactiveObject
+namespace EasySave.ViewModels
 {
+    // ViewModelBase serves as a base class for all view models, providing support for property change notification.
+    public class ViewModelBase : ReactiveObject, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Method to notify listeners that a property value has changed.
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
