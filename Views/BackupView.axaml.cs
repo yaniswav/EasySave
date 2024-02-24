bd2f89a4603961243234e9811994ca83e9c2c015
noano <noan.ouedraogo@gmail.com>
2024-02-24 18:52:58 +0100

    BackupView = OK

--- Views/BackupView.axaml.cs
+++ Views/BackupView.axaml.cs
@@ -13,17 +13,25 @@ public partial class BackupView : UserControl
     public BackupView()
     {
         InitializeComponent();
-        
+
         //Ne fonctionne pas 
         var sampleData = new List<BackupJob>
         {
-            new BackupJob { Name = "Job 1", SourceDirectory = "C:\\Source1", DestinationDirectory = "D:\\Destination1", Type = "Complète", State = "Ready", Progress = "0%" },
-            new BackupJob { Name = "Job 2", SourceDirectory = "C:\\Source2", DestinationDirectory = "D:\\Destination2", Type = "Différentielle", State = "In Progress", Progress = "50%" }
+            new BackupJob
+            {
+                Name = "Job 1", SourceDirectory = "C:\\Source1", DestinationDirectory = "D:\\Destination1",
+                Type = "Complète", State = "Ready", Progress = "0%"
+            },
+            new BackupJob
+            {
+                Name = "Job 2", SourceDirectory = "C:\\Source2", DestinationDirectory = "D:\\Destination2",
+                Type = "Différentielle", State = "In Progress", Progress = "50%"
+            }
             // Add more sample data as needed
         };
         SampleDataGrid.ItemsSource = sampleData;
     }
-    
+
     //Ne fonctionne pas 
     public class BackupJob
     {
@@ -35,7 +43,7 @@ public partial class BackupView : UserControl
         public string State { get; set; }
         public string Progress { get; set; }
     }
-    
+
     private async void OnSelectSourceDirectoryClick(object sender, RoutedEventArgs e)
     {
         var dialog = new OpenFolderDialog();
@@ -55,4 +63,41 @@ public partial class BackupView : UserControl
             TargetDirectoryTextBox.Text = result;
         }
     }
+    
+    private void OnCreateButtonClick(object sender, RoutedEventArgs e)
+    {
+        // TODO: Add logic for creating a backup
+    }
+
+    private void OnEditButtonClick(object sender, RoutedEventArgs e)
+    {
+        // TODO: Add logic for editing a backup
+    }
+
+    private void OnDeleteButtonClick(object sender, RoutedEventArgs e)
+    {
+        // TODO: Add logic for deleting a backup
+    }
+    
+    private void OnExecuteButtonClick(object sender, RoutedEventArgs e)
+    {
+        // TODO: Add logic for deleting a backup
+    }
+    
+    private void OnPauseButtonClick(object sender, RoutedEventArgs e)
+    {
+        // TODO: Add logic for deleting a backup
+    }
+    
+    private void OnPlayButtonClick(object sender, RoutedEventArgs e)
+    {
+        // TODO: Add logic for deleting a backup
+    }
+    
+    private void OnStopButtonClick(object sender, RoutedEventArgs e)
+    {
+        // TODO: Add logic for deleting a backup
+    }
+
 }
+
