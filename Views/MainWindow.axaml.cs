26ec51b16b6ca7f3c930526eb8a01de9584bec65
noano <noan.ouedraogo@gmail.com>
2024-02-24 12:04:34 +0100

    New interface for MainWindow

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -11,16 +11,33 @@ namespace EasySave.Views
         public MainWindow()
         {
             InitializeComponent();
+            UpdateButtonColor();
         }
 
-        private void BackupButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        private void BackupButton_Click(object sender, RoutedEventArgs e)
         {
+            // Logique pour charger BackupView
             MainContent.Content = new BackupView();
+            UpdateButtonColor(activeButton: "Backup");
         }
 
-        private void SettingsButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
         {
+            // Logique pour charger SettingsView
             MainContent.Content = new SettingsView();
+            UpdateButtonColor(activeButton: "Settings");
         }
+
+        private void UpdateButtonColor(string activeButton = "")
+        {
+            var activeColor = new Avalonia.Media.SolidColorBrush(Avalonia.Media.Color.Parse("#857DFF"));
+            var inactiveColor = new Avalonia.Media.SolidColorBrush(Avalonia.Media.Color.Parse("#B4BAFF"));
+
+            BackupButton.Background = activeButton == "Backup" ? activeColor : inactiveColor;
+            SettingsButton.Background = activeButton == "Settings" ? activeColor : inactiveColor;
+        }
+        
+        
+
     }
 }
