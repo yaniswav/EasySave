e38340afc79d05c7c08c1c5bd8697eac849614f0
noano <noan.ouedraogo@gmail.com>
2024-02-27 10:31:25 +0100

    Added settings button event

--- Views/SettingsView.axaml.cs
+++ Views/SettingsView.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+
 
 namespace EasySave.Views;
 
@@ -9,5 +11,14 @@ public partial class SettingsView : UserControl
     public SettingsView()
     {
         InitializeComponent();
+    }
+    private void OnResetButtonClick(object sender, RoutedEventArgs e)
+    {
+        // TODO: Add logic for resetting settings
+    }
+    
+    private void OnApplyButtonClick(object sender, RoutedEventArgs e)
+    {
+        // TODO: Add logic for applying settings
     }
 }
