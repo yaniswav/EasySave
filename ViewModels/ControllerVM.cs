8117d62fe6f61cd37475780c7514fe0a59c873b7
yaniswav <azellaxmc@gmail.com>
2024-02-29 21:56:34 +0100

    controller VM

---
+++ ViewModels/ControllerVM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EasySave
+{
+    public class BackupJobControlVM
+    {
+        private EasySave.BackupManager _backupManager;
+
+        public BackupJobControlVM(EasySave.BackupManager backupManager)
+        {
+            _backupManager = backupManager;
+        }
+
+        public void PauseJobs(List<string> jobNames)
+        {
+            foreach (var jobName in jobNames)
+            {
+                _backupManager.PauseJob(jobName);
+            }
+        }
+
+        public void ResumeJobs(List<string> jobNames)
+        {
+            foreach (var jobName in jobNames)
+            {
+                _backupManager.ResumeJob(jobName);
+            }
+        }
+
+        public void StopJobs(List<string> jobNames)
+        {
+            foreach (var jobName in jobNames)
+            {
+                _backupManager.StopJob(jobName);
+            }
+        }
+
+        public void StartJobs(List<string> jobNames)
+        {
+            _backupManager.ExecuteJobs(jobNames.ToArray());
+        }
+    }
+}
